727faffe04fd1602bbf9fd10d228aaf19e5d9d20
nishida <k.nishida@vistage-llc.com>
2020-07-31 22:22:24 +0900

    VoteTabコンポーネントをVoteページでimportする

---
+++ src/views/Vote.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <v-row justify="center" align="center">
+            <v-col cols="12">
+                <VoteTab />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import VoteTab from "../components/VoteTab";
+
+export default {
+    name: "Vote",
+    components: {
+        VoteTab,
+    },
+};
+</script>
