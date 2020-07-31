503e0169a91d518fa260d874872868ef1864cbd2
nishida <k.nishida@vistage-llc.com>
2020-07-31 09:52:51 +0900

    ログインページのレイアウトを作る

---
+++ src/views/Login.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-container>
+        <v-row justify="center" align="center">
+            <v-col cols="4"></v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script></script>
