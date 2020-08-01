e070ba27474ff5e1635c11ff864890951afd944b
nishida <k.nishida@vistage-llc.com>
2020-08-01 19:59:30 +0900

    一覧用のページのレイアウトを作る

---
+++ src/views/List.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-container>
+        <v-row justify="center" align="center">
+            <v-col cols="12"></v-col>
+        </v-row>
+    </v-container>
+</template>
+<script></script>
