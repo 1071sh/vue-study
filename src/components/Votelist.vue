ef057b2741ed07412bde8d28c65af27eb414ae1f
nishida <k.nishida@vistage-llc.com>
2020-08-03 13:12:03 +0900

    一覧ページに投票編集ボタンを作成

--- src/components/Votelist.vue
+++ src/components/Votelist.vue
@@ -8,52 +8,39 @@
                     <th class="text-center">
                         技術点
                         <v-btn @click="sortMembersData('tecPoint')" icon>
-                            <v-icon v-if="orderStates.tecPoint.desc"
-                                >mdi-arrow-down</v-icon
-                            >
-                            <v-icon v-if="!orderStates.tecPoint.desc"
-                                >mdi-arrow-up</v-icon
-                            >
+                            <v-icon v-if="orderStates.tecPoint.desc">mdi-arrow-down</v-icon>
+                            <v-icon v-if="!orderStates.tecPoint.desc">mdi-arrow-up</v-icon>
                         </v-btn>
                     </th>
                     <th class="text-center">
                         サービス点
                         <v-btn @click="sortMembersData('servicePoint')" icon>
-                            <v-icon v-if="orderStates.servicePoint.desc"
-                                >mdi-arrow-down</v-icon
-                            >
-                            <v-icon v-if="!orderStates.servicePoint.desc"
-                                >mdi-arrow-up</v-icon
-                            >
+                            <v-icon v-if="orderStates.servicePoint.desc">mdi-arrow-down</v-icon>
+                            <v-icon v-if="!orderStates.servicePoint.desc">mdi-arrow-up</v-icon>
                         </v-btn>
                     </th>
                     <th class="text-center">
                         総合点
                         <v-btn @click="sortMembersData('totalPoint')" icon>
-                            <v-icon v-if="orderStates.totalPoint.desc"
-                                >mdi-arrow-down</v-icon
-                            >
-                            <v-icon v-if="!orderStates.totalPoint.desc"
-                                >mdi-arrow-up</v-icon
-                            >
+                            <v-icon v-if="orderStates.totalPoint.desc">mdi-arrow-down</v-icon>
+                            <v-icon v-if="!orderStates.totalPoint.desc">mdi-arrow-up</v-icon>
                         </v-btn>
                     </th>
+                    <th class="text-center">編集</th>
                 </tr>
             </thead>
             <tbody>
-                <tr
-                    v-for="member in membersDataWithTotalPoint"
-                    :key="`tr-${member.id}`"
-                >
+                <tr v-for="member in membersDataWithTotalPoint" :key="`tr-${member.id}`">
                     <td class="text-center">{{ member.id }}</td>
                     <td class="text-center">{{ member.name }}</td>
-                    <td class="text-center">
-                        {{ member.tecPoint | pointToChars }}
-                    </td>
-                    <td class="text-center">
-                        {{ member.servicePoint | pointToChars }}
-                    </td>
+                    <td class="text-center">{{ member.tecPoint | pointToChars }}</td>
+                    <td class="text-center">{{ member.servicePoint | pointToChars}}</td>
                     <td class="text-center">{{ member.totalPoint }}</td>
+                    <td class="text-center">
+                        <v-btn @click="edit(member.id)" icon>
+                            <v-icon>mdi-pencil-plus-outline</v-icon>
+                        </v-btn>
+                    </td>
                 </tr>
             </tbody>
         </template>
@@ -71,29 +58,29 @@ export default {
                 member.totalPoint = (member.tecPoint + member.servicePoint) / 2;
                 return member;
             });
-        }
+        },
     },
     filters: {
-        pointToChars: point => ["S", "A", "B", "C", "D"][point - 1]
+        pointToChars: point => ["S", "A", "B", "C", "D"][point - 1],
     },
     data: () => ({
         orderStates: {
             tecPoint: {
-                desc: false
+                desc: false,
             },
             servicePoint: {
-                desc: false
+                desc: false,
             },
             totalPoint: {
-                desc: false
-            }
+                desc: false,
+            },
         },
         membersData: new Array(36).fill(null).map((el, index) => ({
             id: index + 1,
             name: `member-${index + 1}`,
             tecPoint: Math.ceil(Math.random() * 5),
-            servicePoint: Math.ceil(Math.random() * 5)
-        }))
+            servicePoint: Math.ceil(Math.random() * 5),
+        })),
     }),
     methods: {
         sortMembersData(sortType) {
@@ -105,10 +92,12 @@ export default {
             });
             this.toggleSortState(sortType);
         },
-
         toggleSortState(sortType) {
             this.orderStates[sortType].desc = !this.orderStates[sortType].desc;
-        }
-    }
+        },
+        edit(id) {
+            console.log(id);
+        },
+    },
 };
 </script>
