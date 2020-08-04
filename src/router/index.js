1174ce6b1b84f91b81b12c8866a579f24af2c7aa
nishida <k.nishida@vistage-llc.com>
2020-08-04 09:44:26 +0900

    ルーティングを定義

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,49 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Home from '../views/Home.vue'
+import Login from "../views/Login.vue";
+import Vote from "../views/Vote.vue";
+import List from "../views/List.vue";
+import ProductInfo from "../views/ProductInfo.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const routes = [
+    {
+        path: "/",
+        name: "Login",
+        component: Login
+    },
+    {
+        path: "/vote",
+        name: "Vote",
+        component: Vote
+    },
+    {
+        path: "/list",
+        name: "List",
+        component: List
+    },
+    {
+        path: "/productInfo",
+        name: "ProductInfo",
+        component: ProductInfo
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/About.vue")
+    }
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
+});
 
-export default router
+export default router;
